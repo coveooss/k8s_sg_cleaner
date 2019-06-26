df0f96e409c8ad1da5a814809d3464a128aa3157
Thomas <tdegiacinto@coveo.com>
2019-06-26 11:15:27 -0400

    quick fix
    - auto detect region
    - exit on error

--- check_k8s_sg.py
+++ check_k8s_sg.py
@@ -3,16 +3,18 @@ import click
 from kubernetes import config
 import logging
 import urllib.request
+import json
+
 
 def get_k8s_sg(cluster_name, region, groupname_filter='k8s-elb'):
-    ec2 = boto3.client('ec2',region_name=region)
+    ec2 = boto3.client('ec2', region_name=region)
     security_groups = []
 
     sgs = ec2.describe_security_groups(
         Filters=[
             {
-                'Name':'tag:KubernetesCluster',
-                'Values':[cluster_name]
+                'Name': 'tag:KubernetesCluster',
+                'Values': [cluster_name]
             }
         ]
     )
@@ -22,9 +24,11 @@ def get_k8s_sg(cluster_name, region, groupname_filter='k8s-elb'):
             security_groups.append(sg['GroupId'])
     return set(security_groups)
 
+
 def get_k8s_cluster():
     contexts = config.list_kube_config_contexts()
     return contexts[1]['context']['cluster']
+
 
 def get_inuse_elb_sg(region):
     continue_iter = True
@@ -50,17 +54,20 @@ def get_inuse_elb_sg(region):
 
 
 @click.command()
-@click.option("--region", "-r", default=None,help="Region where to run the script")
-@click.option("--cluster_name", default=None,help="Specify the k8s cluster name.")
+@click.option("--region", "-r", default=None, help="Region where to run the script")
+@click.option("--cluster_name", default=None, help="Specify the k8s cluster name.")
 def clean_sg(region, cluster_name):
 
     # Check region
     if not region:
         try:
-            region = urllib.request.urlopen('http://169.254.169.254/latest/dynamic/instance-identity/document', timeout=2).read().decode()['region']
+            metadata = urllib.request.urlopen(
+                'http://169.254.169.254/latest/dynamic/instance-identity/document', timeout=2).read().decode()
+            region = json.loads(metadata)['region']
         except:
             region = boto3.session.Session().region_name
-            logging.warning('Cannot detect region from metadata, use default region({})'.format(region))
+            logging.warning(
+                'Cannot detect region from metadata, use default region({})'.format(region))
 
     ec2 = boto3.client('ec2', region_name=region)
     ec2r = boto3.resource('ec2', region_name=region)
@@ -70,7 +77,9 @@ def clean_sg(region, cluster_name):
         try:
             cluster_name = get_k8s_cluster()
         except:
-            logging.error('Cannot load cluster name from kubeconfig or cluster name option not specify')
+            logging.error(
+                'Cannot load cluster name from kubeconfig or cluster name option not specify')
+            exit(1)
 
     # Get all sg attach to running service in k8s
     elb_sgs = get_inuse_elb_sg(region)
@@ -81,7 +90,8 @@ def clean_sg(region, cluster_name):
     sg_to_delete = k8s_sg - elb_sgs
 
     if sg_to_delete:
-        logging.info("{} - {} sg are currently in use by k8s services, {} will be deleted.".format(cluster_name, len(elb_sgs),len(sg_to_delete)))
+        logging.info("{} - {} sg are currently in use by k8s services, {} will be deleted.".format(
+            cluster_name, len(elb_sgs), len(sg_to_delete)))
 
         for sg in sg_to_delete:
             sg_def = ec2r.SecurityGroup(sg)
@@ -90,9 +100,13 @@ def clean_sg(region, cluster_name):
                 ec2.delete_security_group(GroupId=sg)
                 logging.info('{} deleted'.format(sg))
             except Exception as e:
-                    logging.error('Unable to delete following SG : {}: {}'.format(sg, e))
+                logging.error(
+                    'Unable to delete following SG : {}: {}'.format(sg, e))
+    else:
+        logging.info('There is no leaked security group')
+
 
 if __name__ == '__main__':
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
     clean_sg()
-
